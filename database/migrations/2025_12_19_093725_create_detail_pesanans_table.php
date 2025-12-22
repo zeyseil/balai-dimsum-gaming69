8cc6ddf0f503b59c938927b91ac43275f39cc3e7
ameriyana <eriyanutt354@gmail.com>
2025-12-22 20:01:31 +0700

    o

--- database/migrations/2025_12_19_093725_create_detail_pesanans_table.php
+++ database/migrations/2025_12_19_093725_create_detail_pesanans_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('detail_pesanan', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('pesanan_id')->constrained('pesanan')->onDelete('cascade');
+            $table->foreignId('pelanggan_id')->constrained('pelanggan')->onDelete('cascade');
             $table->foreignId('menu_id')->constrained('menu')->onDelete('cascade');
             $table->integer('jumlah_pesanan');
             $table->decimal('harga_satuan',10,2);
